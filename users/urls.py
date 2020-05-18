a5bb9f52ea47c28dc9ea1af16603758d96bc5d5e
William Rodrigues <williamrodrigues2442@gmail.com>
2020-05-17 23:47:39 -0300

    users can now change their password

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 """URL patterns for users app."""
 
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -9,17 +9,30 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    # ex: /users/login/
+    # Ex: /users/login/
     path(
-        'login/', 
+        'login/',
         auth_views.LoginView.as_view(template_name='users/login.html'),
-        name='login',
-    ),
+        name='login'),
 
-    # ex: /users/logout/
+    # Ex: /users/logout/
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # ex: /users/register/
+    # Ex: /users/password_change/
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='users/password_change.html',
+            success_url=reverse_lazy('users:password_change_done')),
+        name='password_change'),
+
+    # Ex: /users/password_change_done/
+    path(
+        'password_change_done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
+        name='password_change_done'),
+
+    # Ex: /users/register/
     path('register/', views.register, name='register'),
 ]
-
